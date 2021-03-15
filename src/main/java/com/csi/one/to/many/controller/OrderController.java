9edf94fa40720dc38dbbf7628e8b0a01ffb6c670
prajul-shinde <prajulshinde@gmail.com>
2021-03-15 15:16:28 +0530

    refractored controller to return proper response

--- src/main/java/com/csi/one/to/many/controller/OrderController.java
+++ src/main/java/com/csi/one/to/many/controller/OrderController.java
@@ -8,10 +8,14 @@ import com.csi.one.to.many.service.ProductService;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @AllArgsConstructor
@@ -26,21 +30,29 @@ public class OrderController {
 
 
     @PostMapping("/placeOrder")
+    @ResponseStatus(HttpStatus.CREATED)
     public Customer saveOrder(@RequestBody OrderRequest request){
         log.info("inside saveOrder of orderController");
         return customerService.saveOrder(request);
     }
 
     @GetMapping("/findAllOrders")
-    public List<Customer>findAllOrders(){
+    public ResponseEntity<List<Customer>> findAllOrders(){
         log.info("inside findAllOrders of OrdersController");
-        return customerService.findAllOrders();
+        List<Customer>list=customerService.findAllOrders();
+        if(list.size()<=0){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.of(Optional.of(list));
     }
     @GetMapping("/getJoinInfo")
-    public List<OrderResponse> getJoinInformation(){
+    public ResponseEntity<List<OrderResponse>> getJoinInformation(){
         log.info("inside getJoinInformation method of OrderController");
-
-        return customerService.getJoinInformation();
+        List<OrderResponse>list=customerService.getJoinInformation();
+        if(list.size()<=0){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.of(Optional.of(list));
 
     }
 
